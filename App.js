f78b51a2d9a3559e8cfac6e20feb5a710adb5834
Raick Staine <raickstaine@gmail.com>
2025-03-19 13:40:04 -0300

    adicionando cardUser

--- App.js
+++ App.js
@@ -2,17 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, ScrollView, Button, TextInput, View } from 'react-native';
 import Header from './src/components/Header';
 import { useState, useEffect } from 'react';
-import CardUser from './src/components/cardUser';
+import CardUser from './src/components/CardUser';
 
 export default function App() {
 
-  const [users, setUsers] = useState
+  const [users, setUsers] = useState([])
 
   useEffect(() => {
     const fetchUsers = async () => {
       const result = await fetch('http://localhost:3000/user/list')
       const data = await result.json()
       console.log(data)
+      setUsers(data)
     }
     fetchUsers()
   }, [])
@@ -21,7 +22,17 @@ export default function App() {
     <ScrollView style={styles.container}>
       <Header />
       <View style={styles.listUser}>
-        
+        {
+         users.map((user)=>{
+            return <CardUser
+              key={user.id}
+              id={user.id} 
+              name={user.name}
+              email={user.email}
+              avatar={user.avatar}
+            />
+         })
+        }
       </View> 
     </ScrollView>
   );
